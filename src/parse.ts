01f3a84f61f32a854ca3d4686963520f47fe5ad1
James Williams <jamesphwilliams@gmail.com>
2019-11-13 20:22:12 +0000

    Add linting warnings

--- src/parse.ts
+++ src/parse.ts
@@ -4,7 +4,7 @@ import { ChecksUpdateParamsOutputAnnotations } from '@octokit/rest'
 export { getAnnotations }
 
 function getAnnotations(output: string): ChecksUpdateParamsOutputAnnotations[] {
-    const { validation } = JSON.parse(output)
+    const { validation, quality } = JSON.parse(output)
 
     const annotations: ChecksUpdateParamsOutputAnnotations[] = []
 
@@ -21,6 +21,33 @@ function getAnnotations(output: string): ChecksUpdateParamsOutputAnnotations[] {
                 start_line: lineNumber,
                 end_line: lineNumber,
                 annotation_level: 'failure'
+            })
+        })
+    })
+
+    // Only add linting errors if there were no compilation errors:
+    if (annotations.length > 0) {
+        return annotations
+    }
+
+    const linter = quality.runners.find((r: any) => r.runner_key === 'clang-tidy')
+    if (linter === undefined) {
+        return annotations
+    }
+
+    linter.runners.map((runner: any) => {
+        runner.errors.map((error: any) => {
+            const { info, file_path, line_number } = error
+
+            const lineNumber = parseInt(line_number, 10)
+
+            annotations.push({
+                title: "Linter Warning",
+                message: info,
+                path: file_path,
+                start_line: lineNumber,
+                end_line: lineNumber,
+                annotation_level: 'warning'
             })
         })
     })
